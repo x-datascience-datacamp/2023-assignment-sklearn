8e8c5e47fddbfe130598f6fe32eaa832e78a9d1c
nicolassaint <nicolas.saint78@gmail.com>
2023-12-19 17:59:15 +0100

    UP my solution

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -210,7 +210,6 @@ class MonthlySplit(BaseCrossValidator):
         idx_test : ndarray
             The testing set indices for that split.
         """
-
         n_splits = self.get_n_splits(X, y, groups)
         # Group the indexes by month
         X = (X.reset_index()
