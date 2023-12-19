a18cbcd19b4ea9bc56ff04cbbe31344224df0fc1
nasr <nasreh77@gmail.com>
2023-12-19 23:37:28 +0100

    second commit

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -83,7 +83,6 @@ class KNearestNeighbors(BaseEstimator, ClassifierMixin):
         self : instance of KNearestNeighbors
             The current instance of the classifier
         """
-
         # we firstly check that X and y are valid
         X, y = check_X_y(X, y)
         check_classification_targets(y)
@@ -107,7 +106,6 @@ class KNearestNeighbors(BaseEstimator, ClassifierMixin):
         y : ndarray, shape (n_test_samples,)
             Predicted class labels for each test data sample.
         """
-
         check_is_fitted(self)
         X = check_array(X)
 
@@ -164,7 +162,6 @@ class MonthlySplit(BaseCrossValidator):
         for which this column is not a datetime, it will raise a ValueError.
         To use the index as column just set `time_col` to `'index'`.
     """
-
     def __init__(self, time_col="index"):  # noqa: D107
         self.time_col = time_col
 
@@ -216,7 +213,6 @@ class MonthlySplit(BaseCrossValidator):
         idx_test : ndarray
             The testing set indices for that split.
         """
-
         n_splits = self.get_n_splits(X, y, groups)
 
         X = X.reset_index()
