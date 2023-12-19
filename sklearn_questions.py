93b904cd3b1ab95b675c92b0159c41b76e5317b3
louisbetzer <louis.betzer@polytechnique.edu>
2023-12-19 15:24:14 +0100

    just for a line

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -238,7 +238,6 @@ class MonthlySplit(BaseCrossValidator):
         idx_test : ndarray
             The testing set indices for that split.
         """
-
         n_samples = X.shape[0]
         n_splits = self.get_n_splits(X, y, groups)
 
