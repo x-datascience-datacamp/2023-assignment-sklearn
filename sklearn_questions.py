37d740d8a339581f0b20e4664e8b7da6e0051a4b
Yassine BEN JEMAA <yassine.ben_jemaa@telecom-sudparis.eu>
2023-12-21 01:36:25 +0100

    Fixing problems2

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -138,7 +138,7 @@ class KNearestNeighbors(BaseEstimator, ClassifierMixin):
 
 class MonthlySplit(BaseCrossValidator):
     """CrossValidator based on monthly split.
-
+    
     Split data based on the given `time_col` (or default to index). Each split
     corresponds to one month of data for the training and the next month of
     data for the test.
