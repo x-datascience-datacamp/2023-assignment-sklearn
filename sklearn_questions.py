0b8729598a5a46a0e28d3d528f912718c3c0a37f
MattVerlynde <matthieu.verlynde@agroparistech.fr>
2023-12-22 10:06:13 +0100

    UP my solution

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -83,7 +83,6 @@ class KNearestNeighbors(BaseEstimator, ClassifierMixin):
         self : instance of KNearestNeighbors
             The current instance of the classifier
         """
-
         X, y = check_X_y(X, y)
         check_classification_targets(y)
         self.X_, self.y_ = X, y
